f97194e2c1020dcd6f56e686ffc279519105217d
Brandonjca <brandito144@gmail.com>
2022-07-30 16:07:53 -0500

    Ultimo cambio

--- GENERAL/virtual/loginJS.js
+++ GENERAL/virtual/loginJS.js
@@ -1,17 +1,22 @@
 const user = document.getElementById("logUser");
 const pass = document.getElementById("logPass");
 var usuario = JSON.parse(localStorage.getItem("users"));
-function login (event) {
-  event.preventDefault()
- for(var i=0 ; i < localStorage.length ; i++){
-    if (user.value == usuario[i]["user"] && pass.value ==usuario[i]["pass"]) {
-      window.location= "/html/reservas.html"
-    }
-    else if (usuario != null) {
-      alert("su usuario o contraseña son incorrectas")
+
+// function login (event) {
+//   event.preventDefault()
+
+//  for(var i=0 ; i < localStorage.length ; i++){
+//     if (user.value == usuario[i]["user"] && pass.value ==usuario[i]["pass"]) {
+//       window.location= "/html/reservas.html"
+//     }
+//     else if (usuario != null) {
+//       alert("su usuario o contraseña son incorrectas")
       
-    }
-  }
-}
+//     }
+//   }
+
+// }
+
+
 
   
